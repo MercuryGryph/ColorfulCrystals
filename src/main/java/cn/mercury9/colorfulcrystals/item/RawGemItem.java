99f1f75f7efccd3471c73b96950d32ac4eb0d1df
猫猫鹰 <w84627302@gmail.com>
2026-05-16 05:05:58 +0800

    更改切割原石声音

--- src/main/java/cn/mercury9/colorfulcrystals/item/RawGemItem.java
+++ src/main/java/cn/mercury9/colorfulcrystals/item/RawGemItem.java
@@ -4,6 +4,9 @@ import dev.anvilcraft.lib.v2.registrum.util.entry.ItemEntry;
 import dev.anvilcraft.lib.v2.util.nullness.NonNullFunction;
 import dev.anvilcraft.lib.v2.util.nullness.NonNullSupplier;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
@@ -15,6 +18,11 @@ public class RawGemItem extends HoldToTransformItem {
 
     public static NonNullFunction<Properties, RawGemItem> factory(ItemEntry<? extends Item> rawGem) {
         return (properties) -> new RawGemItem(rawGem, properties);
+    }
+
+    protected void playGrindSound(Level level, Player player, BlockPos pos) {
+        final var sound = SoundEvents.UI_STONECUTTER_TAKE_RESULT;
+        level.playSound(player, pos, sound, SoundSource.BLOCKS);
     }
 
     @Override
